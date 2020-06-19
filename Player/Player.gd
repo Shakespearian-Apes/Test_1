f83f3a6d22f69355dd0bb094dba215894d6ba7af
Nicholas Voss <nicholasvoss02@gmail.com>
2020-06-19 10:57:37 -0600

    added player animation

--- Player/Player.gd
+++ Player/Player.gd
@@ -15,18 +15,8 @@ func _ready():
 	pass
 
 func _process(delta):
-	if pick_up == true && Input.is_action_just_pressed("ui_accept") && weapon != null:
-		var gun
-		if weapon.gun == 2:
-			gun = pistol.instance()
-		elif weapon.gun == 1:
-			gun = rifle.instance()
-		if picked_up == true:
-			get_child(3).queue_free()
-		add_child(gun)
-		emit_signal("picked_up", weapon)
-		if picked_up == false:
-			picked_up = true
+	gun_pickup()
+	set_animation()
 
 func _on_Gun_Pickup_area_entered(area):
 	pick_up = true
@@ -36,3 +26,28 @@ func _on_Gun_Pickup_area_entered(area):
 func _on_Gun_Pickup_area_exited(area):
 	pick_up = false
 	weapon = null
+
+func gun_pickup():
+	if pick_up == true && Input.is_action_just_pressed("ui_accept") && weapon != null:
+		var gun
+		if weapon.gun == 2:
+			gun = pistol.instance()
+		elif weapon.gun == 1:
+			gun = rifle.instance()
+		if picked_up == true:
+			get_child(6).queue_free()
+		add_child(gun)
+		emit_signal("picked_up", weapon)
+		if picked_up == false:
+			picked_up = true
+
+func set_animation():
+	var parent = get_parent()
+	if parent.velocity == Vector2.ZERO:
+		$AnimationPlayer.play("idle")
+	elif parent.velocity.x > 0:
+		$Sprite.flip_h = false
+		$AnimationPlayer.play("Walking")
+	elif parent.velocity.x < 0:
+		$Sprite.flip_h = true
+		$AnimationPlayer.play("Walking")
